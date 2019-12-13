104918c9c254c7efeb66ef6940d08542660105d0
Your Name <samanbatool08@gmail.com>
2019-12-13 17:54:27 -0500

    sevenseas lab

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,13 +1,47 @@
 class SeasController < ApplicationController
-  #define your controller actions here
 
+  def welcome 
+    render :welcome
+  end
+
+  def index
+    @seas = Sea.all
+
+    render :index
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+    render :show
+  end
+
+  def new
+    @sea = Sea.new
+    render :new
+  end
+
+  def create
+    @sea = Sea.create(sea_params)
+    redirect_to sea_path(@sea)
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+    @sea.update(sea_params)
+
+    redirect_to sea_path(@sea)
+
+  end
 
   private
-  # In controller actions, use this private method to access sea params from forms.
-  # Example: @sea.update(sea_params)
-  # check the return value of sea_params to see what you're working with!
+
   def sea_params
-    params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
-  end
+    params.require(:sea).permit(:name, :temperature, :bio, :image_url, :mood, :favorite_color, :scariest_creature, :has_mermaids)
+end
 
 end
--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :seas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :seas, only: [:index, :new, :show, :edit, :update]
+
+# get '/', to: 'seas#welcome'
+# root to: 'seas#welcome'
+root 'seas#welcome'
 
 end
